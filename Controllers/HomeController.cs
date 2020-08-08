0ae37de4d3ef2c621e7ec7e6dff20bfc0606e228
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-08-08 16:40:37 -0400

    completed assignment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            return View("Success");
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid) 
+            {
+                return RedirectToAction("Success");
+            }
+
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
